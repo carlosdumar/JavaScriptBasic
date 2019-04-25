8e2efadeb4a194d4e2f60762131084d26c78094e
Carlos Rodriguez <crodriguez@nolo.com>
2019-04-25 16:39:09 -0500

    commit 2

---
+++ clase2.js
@@ -0,0 +1,9 @@
+var nombre = 'sacha', apellido = 'lifsyc'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLocaleLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = 
